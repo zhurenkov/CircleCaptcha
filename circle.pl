6e6dab567beb3a91726efe7136d1bbb3883f8e93
zhur <zhur@pie-aael.ru>
2021-07-28 13:20:44 +0700

    Добавлено описание на русском.

--- circle.pl
+++ circle.pl
@@ -8,17 +8,18 @@ open CAP, ">cap.svg" or die "Невозможно создать SVG-файл: $
  
 our $m=100;
 # create an SVG object with a size of 200x200 pixels
+# Создаём SVG объект размером 200 на 200 пикселей.
 my $svg = SVG->new(
 width => $m*2,
 height => $m*2,
 );
 
 our $n=shift()+1;
-#$n=10;
 my $r;
 my %rgb;
 
 # add a circles
+# Создаём случайные круги.
 for(my $i=0;$i<$n;$i++){
 $r=10+floor(rand()*($m-10));
 %rgb=(R=>floor(rand()*255), G=>floor(rand()*255), B=>floor(rand()*255));
@@ -29,7 +30,6 @@ cx => $r+floor(rand()*2*($m-$r)),
 cy => $r+floor(rand()*2*($m-$r)),
 style => {
  'fill' => 'rgb('.$rgb{R}.','.$rgb{G}.','.$rgb{B}.')',
-# 'fill' => 'rgb(255, 0, 0)',
  'stroke' => 'black',
  'stroke-width' => 1,
  'stroke-opacity' => .5,
@@ -37,9 +37,9 @@ style => {
 },
 );
 print $r." ";
-print $rgb{R}."+".$rgb{G}."+".$rgb{B}." ";
+print $rgb{R}."+".$rgb{G}."+".$rgb{B}."; ";
 }
 
 # now render the SVG object, implicitly use svg namespace
-#print $svg->xmlify;
+# Рендерим SVG объект и превращаем его в svg-файл.
 print CAP $svg->xmlify;
